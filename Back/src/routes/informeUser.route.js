72d8530117b584c498d99fea92dd84d1e861b46e
kren007 <70988157+kren007@users.noreply.github.com>
2023-06-22 20:21:32 -0600

    update informe usuario :bar_chart:

--- Back/src/routes/informeUser.route.js
+++ Back/src/routes/informeUser.route.js
@@ -4,6 +4,6 @@ const router = Router()
 const loginController = require('../controllers/informeUser.controller')
 const verifyToken = require('../middlewares/verifyToken')
 router.post('/informeUser', verifyToken, loginController.informeUser)
-
+router.post('/totalUser', verifyToken, loginController.totalUser)
 
 module.exports = router
