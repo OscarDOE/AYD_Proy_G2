eaefd4ee06d9bc321ce12dc4d70224781589e030
kren007 <70988157+kren007@users.noreply.github.com>
2023-06-30 23:31:50 -0600

    update navegacion :hammer:

--- Back/src/controllers/pedido.controller.js
+++ Back/src/controllers/pedido.controller.js
@@ -177,13 +177,13 @@ const getMenu = async (req, res) => {
     const { idEmp } = req.body
     try {
         // Obtiene las empresas
-        let query1 = "SELECT c.id AS id, c.nombre AS nombre, c.precio AS precio "
+        let query1 = "SELECT c.id AS id, c.nombre AS nombre, c.precio AS precio, c.imagen as imagen, c.descripcion as descripcion"
         query1 += "FROM empresa e "
         query1 += "LEFT JOIN menu m ON e.usuario_id = m.empresa_usuario_id "
         query1 += "LEFT JOIN combo c ON m.id = c.menu_id "
         query1 += "WHERE e.usuario_id = ? "
         query1 += "UNION "
-        query1 += "SELECT p.id AS producto_id, p.nombre AS producto_nombre, p.precio AS producto_precio "
+        query1 += "SELECT p.id AS producto_id, p.nombre AS producto_nombre, p.precio AS producto_precio, p.imagen as imagen, p.descripcion as descripcion"
         query1 += "FROM empresa e "
         query1 += "LEFT JOIN menu m ON e.usuario_id = m.empresa_usuario_id "
         query1 += "LEFT JOIN producto p ON m.id = p.menu_id "
