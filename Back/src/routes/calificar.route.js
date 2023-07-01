696a65ce00109a39e4abca036dbb32466e128136
kren007 <70988157+kren007@users.noreply.github.com>
2023-06-30 22:03:11 -0600

    update cliente :cactus:

--- Back/src/routes/calificar.route.js
+++ Back/src/routes/calificar.route.js
@@ -3,7 +3,7 @@ const router = Router()
 
 const loginController = require('../controllers/calificar.controller')
 const verifyToken = require('../middlewares/verifyToken')
-router.post('/calificar', verifyToken, loginController.calificar)
+router.post('/calificacion', verifyToken, loginController.calificacion)
 
 
 module.exports = router
