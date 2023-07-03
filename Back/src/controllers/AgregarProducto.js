fc3a4d9c79c15c382cbd1613bf59944655a735b2
JBastian29 <3656535780101@ingenieria.usac.edu.gt>
2023-07-02 21:42:12 -0600

    retoques al back

--- Back/src/controllers/AgregarProducto.js
+++ Back/src/controllers/AgregarProducto.js
@@ -98,143 +98,7 @@ const AgregarProducto = async (req, res) => {
   }
 };
 
-const EditarProducto = async (req, res) => {
-  const { nombre, nuevoPrecio, nuevaDisponibilidad } = req.body;
 
-  if (!nombre || !nuevoPrecio || !nuevaDisponibilidad) {
-    return res.status(400).json({
-      status: "FAILED",
-      data: {
-        error:
-          "El nombre, nuevo precio y nueva disponibilidad del producto son obligatorios",
-      },
-      message:
-        "El nombre, nuevo precio y nueva disponibilidad del producto son obligatorios",
-    });
-  }
-
-  try {
-    // Verificar si el producto existe en la base de datos
-    const existingProduct = await query(
-      "SELECT * FROM producto WHERE nombre = ?",
-      [nombre]
-    );
-
-    if (existingProduct.length === 0) {
-      return res.status(404).json({
-        status: "FAILED",
-        message: "El producto no existe",
-      });
-    }
-
-    // Actualizar el producto en la base de datos
-    const updateQuery = `
-      UPDATE producto
-      SET precio = ?, disponibilidad = ?
-      WHERE nombre = ?
-    `;
-    await query(updateQuery, [nuevoPrecio, nuevaDisponibilidad, nombre]);
-
-    res.status(200).json({
-      status: "OK",
-      message: "Producto actualizado correctamente",
-    });
-  } catch (error) {
-    console.error("ERROR - EditarProducto", error);
-
-    res.status(500).json({
-      status: "FAILED",
-      message: "Error al editar producto",
-    });
-  }
-};
-
-const EliminarProducto = async (req, res) => {
-  const { nombreProducto } = req.body;
-
-  if (!nombreProducto) {
-    return res.status(400).json({
-      status: "FAILED",
-      data: {
-        error: "El nombre del producto es obligatorio",
-      },
-      message: "El nombre del producto es obligatorio",
-    });
-  }
-
-  try {
-    // Verificar si el producto existe en la base de datos
-    const existingProduct = await query(
-      "SELECT * FROM producto WHERE nombre = ?",
-      [nombreProducto]
-    );
-
-    if (existingProduct.length === 0) {
-      return res.status(404).json({
-        status: "FAILED",
-        message: "El producto no existe",
-      });
-    }
-
-    // Eliminar el producto de la base de datos
-    const deleteQuery = "DELETE FROM producto WHERE nombre = ?";
-    await query(deleteQuery, [nombreProducto]);
-
-    res.status(200).json({
-      status: "OK",
-      message: "Producto eliminado correctamente",
-    });
-  } catch (error) {
-    console.error("ERROR - EliminarProducto", error);
-
-    res.status(500).json({
-      status: "FAILED",
-      message: "Error al eliminar producto",
-    });
-  }
-};
-
-const RealizarPedido = async (req, res) => {
-  const { nombreProducto } = req.body;
-
-  if (!nombreProducto) {
-    return res.status(400).json({
-      status: "FAILED",
-      data: {
-        error: "El nombre del producto es obligatorio",
-      },
-      message: "El nombre del producto es obligatorio",
-    });
-  }
-
-  try {
-    // Verificar si el producto existe en la base de datos
-    const existingProduct = await query(
-      "SELECT * FROM producto WHERE nombre = ?",
-      [nombreProducto]
-    );
-
-    if (existingProduct.length === 0) {
-      return res.status(404).json({
-        status: "FAILED",
-        message: "El producto no existe",
-      });
-    }
-
-    res.status(200).json({
-      status: "OK",
-      message: "Pedido realizado correctamente",
-      data: existingProduct[0],
-    });
-  } catch (error) {
-    console.error("ERROR - RealizarPedido", error);
-
-    res.status(500).json({
-      status: "FAILED",
-      message: "Error al realizar pedido",
-    });
-  }
-};
 
 const ObtenerProductos = async (req, res) => {
   const {
@@ -260,8 +124,8 @@ const ObtenerProductos = async (req, res) => {
 
 module.exports = {
   ObtenerProductos,
-  AgregarProducto,
-  EditarProducto,
-  EliminarProducto,
-  RealizarPedido,
+  AgregarProducto
+  // EditarProducto,
+  // EliminarProducto,
+  // RealizarPedido,
 };
